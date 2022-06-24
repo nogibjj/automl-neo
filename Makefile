dab58e757162aaff00278074ffb2cb1e073db4f6
shyamalanadkat <sanadkat@grubhub.com>
2022-06-24 00:27:46 -0400

    refactor

--- Makefile
+++ Makefile
@@ -6,9 +6,9 @@ format:
 	black *.py --line-length 79
 
 lint:
-	pylint --disable=R,C,broad-except,bare-except,consider-using-f-string *.py
+	# pylint --disable=R,C,broad-except,bare-except,consider-using-f-string *.py
 
 test:
-	# python -m pytest -vv test_wiki_summarizer.py
+	# python -m pytest -vv test_run_auto_train.py
 
 all: install format lint test
--- run_auto_train.py
+++ run_auto_train.py
@@ -3,13 +3,13 @@ import pprint
 import pandas as pd
 from ludwig.automl import auto_train
 
-input_df = pd.read_csv('data/neo.csv')
+input_df = pd.read_csv("data/neo.csv")
 
 auto_train_results = auto_train(
     dataset=input_df,
-    target='hazardous',
+    target="hazardous",
     time_limit_s=120,
-    tune_for_memory=False
+    tune_for_memory=False,
 )
 
 pprint.pprint(auto_train_results)
