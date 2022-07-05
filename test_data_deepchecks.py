322a716e8f626c27ee02c5d105c4cbb4de0f183d
shyamalanadkat <sanadkat@grubhub.com>
2022-07-05 17:18:41 -0400

    formatting

--- test_data_deepchecks.py
+++ test_data_deepchecks.py
@@ -6,11 +6,11 @@ import pandas as pd
 def test_data_integrity():
     # The label can be passed as a column name or a separate pd.Series / pd.DataFrame
     df = pd.read_csv("data/neo.csv")
-    ds = Dataset(df, label='hazardous')
+    ds = Dataset(df, label="hazardous")
     # Run Suite
     integ_suite = data_integrity().remove(0)
     suite_result = integ_suite.run(ds)
     # Note: the result can be saved as html using suite_result.save_as_html()
     # or exported to json using suite_result.to_json()
-    suite_result.save_as_html(file='deepchecks.html')
+    suite_result.save_as_html(file="deepchecks.html")
     assert len(suite_result.get_not_passed_checks()) == 0
